eb71f573a564562257e30484a7d4c0c3acb0a04d
Reza Jelveh <toyb0mb@gmx.net>
2009-01-13 15:05:36 +0100

    easy peasy pxelinux netboot with dnsmasq and osx

---
+++ dnsmasq_tftp_osx.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# set ethernet to 192.168.2.1 in sharing preferences
+DNSMASQCONF=/opt/local/etc/dnsmasq.conf
+
+sudo port install dnsmasq
+
+sudo cp /opt/local/etc/dnsmasq.conf.sample $DNSMASQCONF
+sudo perl -i -pe 's/^#dhcp-range=192.168.0.50,192.168.0.150,255.255.255.0,12h/dhcp-range=192.168.2.50,192.168.2.150,255.255.255.0,12h/' $DNSMASQCONF
+sudo perl -i -pe 's/^#dhcp-boot=pxelinux.0/dhcp-boot=pxelinux.0/' $DNSMASQCONF
+sudo perl -i -pe 's/^#enable-tftp/enable-tftp/' $DNSMASQCONF
+sudo perl -i -pe 's/^#tftp-root=\/var\/ftpd/tftp-root=\/opt\/local\/var\/tftpboot/' $DNSMASQCONF
+echo '127.0.0.1 archive.ubuntu.com' >> /etc/hosts
+sudo cp -r netboot/* /opt/local/var/tftpboot
+sudo /opt/local/sbin/dnsmasq -d
