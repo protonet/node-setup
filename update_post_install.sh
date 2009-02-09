d591223a9df14b0a4cf86fc71f4dc7227657132d
Reza Jelveh <toyb0mb@gmx.net>
2009-02-09 12:53:12 +0100

    made sure preseed points to deploy.flying and added the preseed to the deployment automation

--- update_post_install.sh
+++ update_post_install.sh
@@ -5,5 +5,6 @@ HOST=protodeploy@deploy.flyingseagull.de
 tar cjvf post_install.tar.bz2 post_install
 scp post-install $HOST:deploy/assets
 scp post_install.tar.bz2 $HOST:deploy/assets
+scp netboot/install/preseed/preseed.cfg $HOST:deploy/assets
 
 rm post_install.tar.bz2
