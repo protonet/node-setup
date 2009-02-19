24e313996f536c6188fde78add827e1b86f5d8b4
ali.local <Ali@alibook-9.local>
2009-02-19 22:15:32 +0100

    starting to add all the ptn dashboard stuff

--- dnsmasq_tftp_osx.sh
+++ dnsmasq_tftp_osx.sh
@@ -10,6 +10,11 @@ sudo perl -i -pe 's/^#dhcp-range=192.168.0.50,192.168.0.150,255.255.255.0,12h/dh
 sudo perl -i -pe 's/^#dhcp-boot=pxelinux.0/dhcp-boot=pxelinux.0/' $DNSMASQCONF
 sudo perl -i -pe 's/^#enable-tftp/enable-tftp/' $DNSMASQCONF
 sudo perl -i -pe 's/^#tftp-root=\/var\/ftpd/tftp-root=\/opt\/local\/var\/tftpboot/' $DNSMASQCONF
-echo '127.0.0.1 archive.ubuntu.com' >> /etc/hosts
+sudo -e echo '127.0.0.1 archive.ubuntu.com' >> /etc/hosts
+# sudo mkdir -p /opt/local/var/tftpboot
+# sudo cp -r netboot/ /opt/local/var/tftpboot
 sudo cp -r netboot/* /opt/local/var/tftpboot
+if [ ! -e /opt/local/var/db ]; then
+  sudo mkdir /opt/local/var/db;
+fi;
 sudo /opt/local/sbin/dnsmasq -d
