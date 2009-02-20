17f677f77fb896f06136144606954116e0203e33
Reza Jelveh <>
2009-02-20 23:52:15 +0100

    changed update post install to work locally

--- update_post_install.sh
+++ update_post_install.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-HOST=protodeploy@deploy.flyingseagull.de
-
+cd /opt/local/var/tftpboot/install/deploy_root/
 # for os x tells tar not to include resource forks
 export COPYFILE_DISABLE=true
 tar cjvf post_install.tar.bz2 post_install
-scp post-install $HOST:deploy/assets
-scp post_install.tar.bz2 $HOST:deploy/assets
-scp netboot/install/preseed/preseed.cfg $HOST:deploy/assets
-
-rm post_install.tar.bz2
