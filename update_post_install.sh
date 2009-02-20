3754df82fca3bf3c45a3b414ae149c7a0bc4e0ca
ali.local <Ali@alibook-9.local>
2009-02-20 21:04:44 +0100

    fixing resource forks being included for os x

--- update_post_install.sh
+++ update_post_install.sh
@@ -2,6 +2,8 @@
 
 HOST=protodeploy@deploy.flyingseagull.de
 
+# for os x tells tar not to include resource forks
+export COPYFILE_DISABLE=true
 tar cjvf post_install.tar.bz2 post_install
 scp post-install $HOST:deploy/assets
 scp post_install.tar.bz2 $HOST:deploy/assets
