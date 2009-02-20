b0fc0a0405f9d56c18f83d495727d078fac60dc4
Reza Jelveh <>
2009-02-20 23:13:35 +0100

    restructured dnsmasq_tftp a bit after adding lighttpd to it

--- dnsmasq_tftp_osx.sh
+++ dnsmasq_tftp_osx.sh
@@ -3,18 +3,25 @@
 # set ethernet to 192.168.2.1 in sharing preferences
 DNSMASQCONF=/opt/local/etc/dnsmasq.conf
 
-sudo port install dnsmasq
+# sudo cp /opt/local/etc/dnsmasq.conf.sample $DNSMASQCONF
+# sudo perl -i -pe 's/^#dhcp-range=192.168.0.50,192.168.0.150,255.255.255.0,12h/dhcp-range=192.168.2.50,192.168.2.150,255.255.255.0,12h/' $DNSMASQCONF
+# sudo perl -i -pe 's/^#dhcp-boot=pxelinux.0/dhcp-boot=pxelinux.0/' $DNSMASQCONF
+# sudo perl -i -pe 's/^#enable-tftp/enable-tftp/' $DNSMASQCONF
+# sudo perl -i -pe 's/^#tftp-root=\/var\/ftpd/tftp-root=\/opt\/local\/var\/tftpboot/' $DNSMASQCONF
 
-sudo cp /opt/local/etc/dnsmasq.conf.sample $DNSMASQCONF
-sudo perl -i -pe 's/^#dhcp-range=192.168.0.50,192.168.0.150,255.255.255.0,12h/dhcp-range=192.168.2.50,192.168.2.150,255.255.255.0,12h/' $DNSMASQCONF
-sudo perl -i -pe 's/^#dhcp-boot=pxelinux.0/dhcp-boot=pxelinux.0/' $DNSMASQCONF
-sudo perl -i -pe 's/^#enable-tftp/enable-tftp/' $DNSMASQCONF
-sudo perl -i -pe 's/^#tftp-root=\/var\/ftpd/tftp-root=\/opt\/local\/var\/tftpboot/' $DNSMASQCONF
-sudo -e echo '127.0.0.1 archive.ubuntu.com' >> /etc/hosts
-# sudo mkdir -p /opt/local/var/tftpboot
-# sudo cp -r netboot/ /opt/local/var/tftpboot
+sudo port install dnsmasq
+sudo port install lighttpd
+
+# lighty stuff
+sudo mkdir -p /opt/local/var/lighttpd/log
+sudo cp conf/lighttpd.conf /opt/local/etc/lighttpd
+
+sudo cp conf/dnsmasq.conf $DNSMASQCONF
+
+sudo mkdir -p /opt/local/var/tftpboot
 sudo cp -r netboot/* /opt/local/var/tftpboot
 if [ ! -e /opt/local/var/db ]; then
   sudo mkdir /opt/local/var/db;
 fi;
+
 sudo /opt/local/sbin/dnsmasq -d
