2651a6383b15cb184ce0a940e5b6b18571e20db6
Reza Jelveh <toyb0mb@gmx.net>
2009-03-21 21:54:47 +0100

    removed static post-install it will now be generated by update-whatever

--- update_post_install.sh
+++ update_post_install.sh
@@ -1,6 +1,24 @@
-#!/bin/sh
+#!/bin/bash
+
+# the archive of the post-install scripts
+POST_ARCH=post_install.tar.bz2
+POST_SERV="http://protodeploy/$POST_ARCH"
+POST_DIR=post_install
 
 cd /opt/local/var/tftpboot/install/deploy_root/
 # for os x tells tar not to include resource forks
 export COPYFILE_DISABLE=true
-tar cjvf post_install.tar.bz2 post_install
+tar cjvf iptables.tar.bz2 post_install
+
+# TODO: sign the releases so we can make sure ppl don't put malicious code
+# in our deployment scripts
+
+rm post-install
+shopt -s extglob
+for post_install in $POST_DIR/+(S[0-9][0-9]*[^~]) ; do
+  cat $post_install >> post-install
+done
+
+
+# remove the temporary files
+# rm -rf $POST_DIR
